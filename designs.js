8c94efa60fbd5e0a5f8a992703f7f0a2d8d72a1d
dtuck20 <30454354+dtuck20@users.noreply.github.com>
2018-01-28 04:03:26 -0700

    Notes and indentation

--- designs.js
+++ designs.js
@@ -6,24 +6,27 @@ var cell = $("<td></td>");
 var submit = $("input[type=submit]");
 
 var grid = $("#pixelCanvas");
+
+
 // When size is submitted by the user, call makeGrid()
 submit.on("click", function makeGrid() {
-var gHeight = $("input#inputHeight").val();
-var gWidth = $("input#inputWeight").val();
+  var gHeight = $("input#inputHeight").val();
+  var gWidth = $("input#inputWeight").val();
+// prevent the page from reloading
+  event.preventDefault();
+// clear previous grid
+  $("#pixelCanvas").children().remove();
 
-event.preventDefault();
-$("#pixelCanvas").children().remove();
-
-for (var r=0; r < gHeight; r++) {
-  $(grid).append(row.clone().each(function() {
-      for (var c = 0; c < gWidth; c++) {
-        $(this).append(cell.clone());
+  for (var r=0; r < gHeight; r++) {
+    $(grid).append(row.clone().each(function() {
+        for (var c = 0; c < gWidth; c++) {
+          $(this).append(cell.clone());
       }
   }))
 }
 })
 
-
+// Paint the cell if it's clicked
 grid.mouseover(function() {
   $(this).find("td").on("click", function() {
     $(this).css("background-color", color.val());
